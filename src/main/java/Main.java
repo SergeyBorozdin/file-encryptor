e079b8f0cd9c5401b7a6096ae9cad629b557f12d
Sergey Borozdin <borozdinsm@yandex.ru>
2022-07-15 00:08:49 +0300

    commit question

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
+import net.lingala.zip4j.ZipFile;// важно импорт класса ZipFile из библиотеки maven
+import net.lingala.zip4j.model.ZipParameters;
+
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args)
     {
+        String path = "C:\\Users\\60034452\\Desktop\\";
+        ZipParameters parameters = new ZipParameters(); // создаем параметры
+        parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
 
+        try {
+            ZipFile zipFile = new ZipFile(path + "archive.zip");
+            zipFile.addFile(
+                    new File(path + "picture.jpeg"), // передаем путь
+                    parameters // передаем параметры
+            );
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
