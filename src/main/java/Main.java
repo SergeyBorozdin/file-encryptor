a88c447aa6182663165351f9505507d29c6c3e42
Sergey Borozdin <borozdinsm@yandex.ru>
2022-07-27 22:23:49 +0300

    feature#3 adding a folder to the archive using the library maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,15 +11,15 @@ public class Main {
     {
         String path = "C:\\Users\\60034452\\Desktop\\";
         ZipParameters parameters = new ZipParameters(); // создаем параметры
-        parameters.setCompressionMethod(CompressionMethod.DEFLATE);
+        parameters.setCompressionMethod(CompressionMethod.DEFLATE);// тип энкриптора или тип архивации
         //parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
         //parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_ULTRA);
-        parameters.setCompressionLevel(CompressionLevel.HIGHER);
+        parameters.setCompressionLevel(CompressionLevel.PRE_ULTRA);// уровень сжатия файлов
 
         try {
             ZipFile zipFile = new ZipFile(path + "archive.zip");
-            zipFile.addFile(
-                    new File(path + "picture.jpeg"), // передаем путь
+            zipFile.addFolder(
+                    new File(path + "folder"), // передаем путь
                     parameters // передаем параметры
             );
         }
