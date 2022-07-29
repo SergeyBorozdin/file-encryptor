c1863379f2ae2b3aa038e2e1978b76a560436201
Sergey Borozdin <borozdinsm@yandex.ru>
2022-07-29 21:44:29 +0300

    feature#7 selecting a folder via the graphical interface

--- src/main/java/GUIForm.java
+++ src/main/java/GUIForm.java
@@ -1,9 +1,55 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
 
 public class GUIForm {
     private JPanel rootPanel;
-    private JTextField textField1;
-    private JButton выберитеФайлИлиПапкуButton;
+    private JTextField filePath;
+    private JButton button;
+    private File selectedFile;
+
+    public GUIForm(){
+        button.addActionListener(new Action() { // позволяет создать обработчик событий
+            @Override
+            public Object getValue(String key) {
+                return null;
+            }
+
+            @Override
+            public void putValue(String key, Object value) {
+
+            }
+
+            @Override
+            public void setEnabled(boolean b) {
+
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+
+            @Override
+            public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+
+            @Override
+            public void removePropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser(); // позволяет выбирать файл
+                chooser.showOpenDialog(rootPanel); // указываем компонет отностильно которого он будет размещен
+                selectedFile = chooser.getSelectedFile();// возвращает нам файл
+                filePath.setText(selectedFile.getAbsolutePath());// вставляем полный путь файла в графическом интерфейсе
+            }
+        });
+    }
 
     public JPanel getRootPanel(){
         return rootPanel;
